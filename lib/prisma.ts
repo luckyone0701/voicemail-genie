cb5fd90f65f42d2ccbcd665907709c2ffff9edbf
luckyone0701 <randybedford320@gmail.com>
2025-12-16 15:57:05 -0600

    Fix Prisma schema + edge client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-};
+const prisma = new PrismaClient();
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+export { prisma };
