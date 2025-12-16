a38eb6cf9cf8a9fcc926f327c1587f3426eebe34
luckyone0701 <randybedford320@gmail.com>
2025-12-16 10:54:08 -0600

    Fix Prisma v7 PrismaClient typing

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,15 @@
 import PrismaClient from "@prisma/client";
 
+const prismaClient = new PrismaClient();
+
+type PrismaClientType = typeof prismaClient;
+
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: PrismaClientType | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
+  globalForPrisma.prisma ?? prismaClient;
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
